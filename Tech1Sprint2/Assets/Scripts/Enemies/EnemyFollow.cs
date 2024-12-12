721da90241248a6cd46c4a22f28fe2c9c933b68f
Mr. Moo42 <GitHub@mrmoo42.com>
2024-12-11 18:16:51 -0800

    Added Two Enemies + More Upgrades
    - Mime enemy which chases the player, and deals damage while touching them.
    - Flying enemy which drops spikes in the mirror world
    - Spikes damage the player when they walk over them.
    - Added upgrades with the remaining sprites we had.

---
+++ Tech1Sprint2/Assets/Scripts/Enemies/EnemyFollow.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public Transform target;
+    private float distance;
+    public float speed = 4.2f;
+    public float damage = 5;
+    private SpriteRenderer sprite;
+
+    private void Awake() {
+        sprite = GetComponent<SpriteRenderer>();
+        if (target == null)
+            target = GameObject.Find("MaterialPlayer").transform;
+    }
+
+    private void Update() {
+        distance = Vector3.Distance(transform.position, target.position);
+        if (target.position.x > transform.position.x) {
+            sprite.flipX = true;
+        } else {
+            sprite.flipX = false;
+        }
+
+
+        if (target != null && distance >= 0.5f) {
+            transform.position = Vector3.Lerp(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision) {
+        GameObject obj = collision.gameObject;
+        if (obj.CompareTag("Player")) {
+            PlayerMovement playerMove = obj.GetComponent<PlayerMovement>();
+            PlayerHealth playerHP = obj.GetComponent<PlayerHealth>();
+            if (!playerMove.invincible) // deal damage only if i frames are inactive
+            {
+                playerHP.Damage(damage);
+                StartCoroutine(playerMove.FlashDamage()); // start flashing and i frames
+            }
+        }
+    }
+}
--- Tech1Sprint2/Assets/Scripts/Upgrades/PlayerUpgradeManager.cs
+++ Tech1Sprint2/Assets/Scripts/Upgrades/PlayerUpgradeManager.cs
@@ -17,28 +17,55 @@ public class PlayerUpgradeManager : MonoBehaviour
             case Upgrade.UpgradeType.MaxHealth:
                 health.maxHealth += n.upgradeValue;
                 upgrades.Add(n);
+                health.Heal(false, n.upgradeValue);
 
                 MaxHealthUI.SetActive(true);
-                UpgradeUIElement uiElement = MaxHealthUI.GetComponent<UpgradeUIElement>();
-                uiElement.amount += 1;
-                uiElement.UpdateUI();
+                UpgradeUIElement maxHPUIElement = MaxHealthUI.GetComponent<UpgradeUIElement>();
+                maxHPUIElement.amount += 1;
+                maxHPUIElement.UpdateUI();
                 
                 break;
             case Upgrade.UpgradeType.MoveSpeed:
                 movement.moveSpeed += n.upgradeValue;
                 upgrades.Add(n);
+
+                MoveSpeedUI.SetActive(true);
+                UpgradeUIElement mvmtUIElement = MoveSpeedUI.GetComponent<UpgradeUIElement>();
+                mvmtUIElement.amount += 1;
+                mvmtUIElement.UpdateUI();
                 break;
             case Upgrade.UpgradeType.AttackDamage:
                 melee.damage += n.upgradeValue;
                 upgrades.Add(n);
+
+                AttackDamageUI.SetActive(true);
+                UpgradeUIElement attkUIElement = AttackDamageUI.GetComponent<UpgradeUIElement>();
+                attkUIElement.amount += 1;
+                attkUIElement.UpdateUI(); 
+                
                 break;
             case Upgrade.UpgradeType.AttackSpeed:
-                melee.attackSpeed -= n.upgradeValue;
+                melee.attackSpeed += n.upgradeValue;
                 upgrades.Add(n);
+                if (melee.attackSpeed < 0.1f) {
+                    melee.attackSpeed = 0.1f;
+                }
+
+                AttackSpeedUI.SetActive(true);
+                UpgradeUIElement atkSpeedUIElement = AttackSpeedUI.GetComponent<UpgradeUIElement>();
+                atkSpeedUIElement.amount += 1;
+                atkSpeedUIElement.UpdateUI();
+
                 break;
             case Upgrade.UpgradeType.AttackRange:
                 melee.attackRange += n.upgradeValue;
                 upgrades.Add(n);
+
+                AttackRangeUI.SetActive(true);
+                UpgradeUIElement rangeUIElement = AttackRangeUI.GetComponent<UpgradeUIElement>();
+                rangeUIElement.amount += 1;
+                rangeUIElement.UpdateUI();
+
                 break;
             default:
                 Debug.LogError(n.Type + " is not recognized as an upgrade type.");
